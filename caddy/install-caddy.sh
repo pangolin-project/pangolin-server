5a417eb550ccc90214e2c83774284bfc94837d27
caddier <45213050+caddier@users.noreply.github.com>
2018-11-24 10:57:55 -0800

    add install server scripts

---
+++ caddy/install-caddy.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+DISTRO='unknown'
+PM='yum'
+DOWNLOAD_URL=https://github.com/pangolin-project/pangolin-server/releases/download/vpg1.0.0/caddy-x86_64-linux.tar.gz
+CONFIG_FILE=./Caddyfile.test
+
+Get_Dist_Name()
+{	
+    if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt-get'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt-get'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt-get'
+    else
+        DISTRO='unknown'
+    fi
+}
+
+Get_Dist_Name 
+echo "you server distribution version  is ${DISTRO}"
+
+type wget >/dev/null 2>&1
+
+if [[ $? -ne 0 ]]; then
+	echo "wget is not install, install it first!!"
+	`${PM} install -y wget`
+	if [[ $? -ne 0 ]]; then
+	    echo "install wget failed. please install wget manually, then run scripts again"
+	    exit -1
+	fi
+
+fi
+echo "### start download caddy server into current directory..."
+wget ${DOWNLOAD_URL} 
+if [[ $? -ne 0 ]]; then
+	echo "down caddy failed ${DOWNLOAD_URL}"
+	exit -1
+fi
+
+echo "### download caddy server finished"
+
+type netstat >/dev/null 2>&1
+
+if [[ $? -ne 0 ]]; then
+	${PM} install -y netstat
+	if [[ $? -ne 0 ]]; then
+		echo "install netstat command failed, please install it manually"
+		exit -1
+	fi
+        netstat -anpt | grep 443 >/dev/nul  2>&1
+	if [[ $? -eq 0  ]];then
+	    echo "443 port is used by other programs. please stop it then run scripts again"
+	    exit -1
+	fi
+fi
+
+
+read -p "please input your domain:" DOMAIN
+read -p "please input your email:" EMAIL
+
+echo "make sure, your 443 port is open and not being used by other programs"
+
+
+
+PASSWORD=`date +%s`
+
+ADMIN_PORT=`date +%s`
+ADMIN_PORT=$((${ADMIN_PORT} % 60000))
+
+touch ${CONFIG_FILE}
+
+echo  "${DOMAIN}:443 {" > ${CONFIG_FILE}
+echo  "    root ./" >> ${CONFIG_FILE}
+echo  "    gzip " >> ${CONFIG_FILE}
+echo  "    log stdout ">> ${CONFIG_FILE}
+echo  "    forwardproxy {" >> ${CONFIG_FILE}
+echo  "        basicauth admin ${PASSWORD}" >> ${CONFIG_FILE}
+echo  "        #adminport ${ADMIN_PORT}" >> ${CONFIG_FILE}
+echo  "        hide_ip" >> ${CONFIG_FILE}
+echo  "        acl {" >> ${CONFIG_FILE}
+echo  "               deny localhost" >> ${CONFIG_FILE}
+echo  "               deny 127.0.0.1" >> ${CONFIG_FILE}
+echo  "            }" >> ${CONFIG_FILE}
+echo  "    }" >> ${CONFIG_FILE}
+echo  "}" >> ${CONFIG_FILE}
+
+type base64 >/dev/null 2>&1
+
+if [[ $? -ne 0 ]];then
+   ${PM} -y install base64
+   if [[ $? -ne 0 ]];then
+	   echo "install base64 command tools failed, pls install it manually"
+	   exit -1
+   fi
+fi
+
+SUFFIX=`echo -n "admin:${PASSWORD}" | base64`
+
+function green(){
+    echo -e "\033[32m $1 \033[0m"
+}
+
+green "connect url is : hs://${SUFFIX}@${DOMAIN}:443/?caddy=1&adp=${ADMIN_PORT}"
+
+
+
